f374f314716a2517f98cf799b84a667e3383612c
rifqi.rt <rifqi.arti.rt@gmail.com>
2016-10-09 18:34:38 +0700

    auth simple siap pakai

--- web/web.js
+++ web/web.js
@@ -3,6 +3,7 @@ var url = require('url');
 var http = require('http');
 var hostname = '127.0.0.1';
 var port = 3000;
+var dbAccount = {};
 
 var server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -10,13 +11,26 @@ var server = http.createServer((req, res) => {
   var parse = url.parse(req.url);
   var api = parse.pathname;
   api = api.split('/');
-  console.log(api);
   if(api[1] == 'auth'){
     if(api[2] == 'read'){
       console.log(QueryStringToJSON(parse.query));
-      res.end('Hello World\n');  
+	  var data = QueryStringToJSON(parse.query);
+	  var resultSha = saltHashPassword(data.username,data.password);
+	  if((resultSha.salt in dbAccount) && ( dbAccount[resultSha.salt] == resultSha.passwordHash)){
+	    res.end('true\n');
+	  } else {
+	    res.end('false\n');
+	  }
     } else if(api[2] == 'create'){
-      
+      console.log(QueryStringToJSON(parse.query));
+	  var data = QueryStringToJSON(parse.query);
+	  if(!(data.username in dbAccount)){
+	    var resultSha = saltHashPassword(data.username,data.password);
+		dbAccount[resultSha.salt] = resultSha.passwordHash;
+		res.end('Account '+ data.username +' Created\n');
+	  } else {
+		res.end('ERROR '+ data.username +' has been created\n');
+	  }
     }
   }
 });
@@ -25,14 +39,20 @@ server.listen(port, hostname, () => {
 });
 var QueryStringToJSON = function (qstring) {            
     var pairs = qstring.split('&');
-    
     var result = {};
     pairs.forEach(function(pair) {
         pair = pair.split('=');
         result[pair[0]] = decodeURIComponent(pair[1] || '');
     });
-
     return JSON.parse(JSON.stringify(result));
+}
+var saltHashPassword = function (salt,userpassword) {
+    //var salt = genRandomString(16); /** Gives us salt of length 16 */
+    var passwordData = sha512(userpassword, salt);
+    console.log('UserPassword = '+userpassword);
+    console.log('Passwordhash = '+passwordData.passwordHash);
+    console.log('\nSalt = '+passwordData.salt);
+	return passwordData;
 }
 var genRandomString = function(length){
     return crypto.randomBytes(Math.ceil(length/2)).toString('hex').slice(0,length);
@@ -46,13 +66,6 @@ var sha512 = function(password, salt){
         passwordHash:value
     };
 };
-function saltHashPassword(userpassword) {
-    var salt = genRandomString(16); /** Gives us salt of length 16 */
-    var passwordData = sha512(userpassword, salt);
-    console.log('UserPassword = '+userpassword);
-    console.log('Passwordhash = '+passwordData.passwordHash);
-    console.log('\nSalt = '+passwordData.salt);
-}
 /*
 username --> md5		a24338a2c5a73e6e
 password --> md5 + salt  	Passwordhash = 61c976b38850260559f77a0f8e1b03b2
